9b0c86f971992137e2ce69903f05ad8cd71aa00d
Carlos Samaniego <cscarlossamaniego@gmail.com>
2019-10-30 11:30:40 -0400

    updates

--- main.py
+++ main.py
@@ -8,16 +8,21 @@ import librosa.feature
 def main():
     all_files = glob.glob('assignment5/data/*.wav') #assignment5/data
     all_files.sort()
-    print(len(all_files))
-
-    data, sampling_rate = librosa.load('./data/203424.wav')
+    # print(len(all_files))
+    data_list = []
+    sampling_rate_list = []
+    for file in all_files:
+        data_list.append(librosa.load(file))
+        sampling_rate_list.append(librosa.load(file))
 
     #will extract 40 Mel-frequency cepstral coefficients
     #these can be used as features to represent each wav file
     #to learn more about MFCCS visit https://en.wikipedia.org/wiki/Mel-frequency_cepstrum
     
-    mfccs = librosa.feature.mfcc(y=data, sr=sampling_rate, n_mfcc=40)
+    mfccs = librosa.feature.mfcc(y=data_list, sr=sampling_rate_list, n_mfcc=40)
+
     mfccsscaled = np.mean(mfccs.T,axis=0)
+    
     print(mfccsscaled)
 
 if __name__ == "__main__":
